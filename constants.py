5c94d85113b65890d73956011b330ecd6968212f
franTarkenton <kevin.netherton@gov.bc.ca>
2021-08-10 10:56:07 -0700

    forgot to add the constants in last commit

--- constants.py
+++ constants.py
@@ -1,7 +1,13 @@
 import dotenv
 import os
 
+# the path to the env file
 envPath = '.env'
 if os.path.exists(envPath):
     print("loading dot env...")
     dotenv.load_dotenv()
+
+
+POSTGRES_USER = os.environ['POSTGRES_USER']
+POSTGRES_PASSWD=os.environ['POSTGRES_PASSWD']
+POSTGRES_INSTANCE=os.environ['POSTGRES_INSTANCE']
