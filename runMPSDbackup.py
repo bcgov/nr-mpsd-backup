a2f67ab1a38b1c307e42e7e27110c2a51361581a
NathanHeidecke <Nathan.Heidecke@gov.bc.ca>
2022-01-10 16:00:57 -0800

    Commit of latest updates to testing branches - environment variables, writing an sql file to local disk

--- runMPSDbackup.py
+++ runMPSDbackup.py
@@ -1,4 +1,11 @@
 # launch file
 
-import logging, os, sys
-print("hello")
+import MPSDbackup2ObjectStore.backupMPSD as backupMPSD
+import datetime, logging, os, sys
+stamp = datetime.datetime.now().strftime("%Y%b%d_%H%M")
+logfile = "logfile_" + stamp + ".log"
+logging.basicConfig(filename=logfile, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.debug("process start - runMPSDbackup.py")
+
+backupMPSD.BackupMPSD().createBackupFile()
